163576bb01f3049dc3897a1e0e32b33cb88e7b0e
andrew <kerovap@mail.ru>
2020-06-22 03:29:35 +0700

    походу застрял

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {createPost} from '../redux/action'
 
-export default class PostForm extends React.Component {
+class PostForm extends React.Component {
     
     constructor(props) {
         super(props)
@@ -18,6 +20,7 @@ export default class PostForm extends React.Component {
         }
 
         console.log(newPost)
+        this.props.createPost(newPost)
         this.setState({title:''})
     }
 
@@ -47,3 +50,9 @@ export default class PostForm extends React.Component {
         )
     }
 }
+
+const mapDispatchToProps = {
+    createPost
+}
+
+export default connect(null,mapDispatchToProps)(PostForm)
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {render} from 'react-dom';
 import App from './App';
-import {createStore} from 'redux'
+import {compose,createStore} from 'redux'
 import * as serviceWorker from './serviceWorker';
 import {Provider} from  'react-redux'
 import { rootReducer } from './redux/rootReducer';
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, compose( 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  ))
 
 const app = (
   <Provider store={store}>
